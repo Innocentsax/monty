576f3f3d1698fa0ed3b7483e784816c10b356298
Owolabi Pius <opius2007@gmail.com>
2023-03-21 19:09:14 +0100

    Monty Team Project

---
+++ pchar.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+* f_pchar - function that prints the char at the top of the stack,
+* followed by a new line
+* @head: stack head
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_pchar(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	h = *head;
+	if (!h)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if (h->n > 127 || h->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", h->n);
+}
