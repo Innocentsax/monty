e87845fac7ae8572cf45a855788cbc2f86ffed7c
Owolabi Pius <opius2007@gmail.com>
2023-03-22 22:12:50 +0100

    Update run_monty.c

--- run_monty.c
+++ run_monty.c
@@ -9,7 +9,6 @@ int run_monty(FILE *script_fd);
 
 /**
  * free_tokens - Frees the global op_toks array of strings.
- * Authors: INNOCENT UDO © innocentcharlesud@gmail.com and PIUS OWOLABI © opius2007@gmail.com
  */
 
 void free_tokens(void)
